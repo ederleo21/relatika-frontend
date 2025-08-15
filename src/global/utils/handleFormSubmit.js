a2396ea5fa1052ac76bf5a1d0e7c07d0052773b0
ederleo <ederordonez10@gmail.com>
2025-08-15 12:30:53 -0500

    Logica de post actualizada

--- src/global/utils/handleFormSubmit.js
+++ src/global/utils/handleFormSubmit.js
@@ -13,19 +13,22 @@ export const handleFormSubmit = async({ values, requestFn, messageSuccess="Opera
 
     }catch(error){
         if (error.response && error.response.data){
+
             const data = error.response.data;
+            //Errores de campos individuales, DRF envia
             setErrors(data);
+            //Errores entre campos, DRF envia
             if (data.non_field_errors) {
                 const message = Array.isArray(data.non_field_errors) ? data.non_field_errors.join(" ") : data.non_field_errors;
                 toast.error(message);
             }
-            if (data.detail) {
-              toast.error(data.detail);
-            }
+            //Errores personalizados de logica
+            if (data.error) toast.error(data.error)
+            //Error de autenticación
+            if (data.detail) toast.error(data.detail);
         } else {
             toast.error("Error inesperado. Intenta más tarde.");
         }
-
     }finally{
         setSubmitting(false);
     }
